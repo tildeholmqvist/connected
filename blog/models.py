9baf967795623ae2ddb85565c465f51aff6672cc
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-13 14:58:42 +0000

    adding the models for comments

--- blog/models.py
+++ blog/models.py
@@ -37,12 +37,13 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    apporoved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_at"]
     
     def __str__(self): 
         return f"{self.body} by {self.author}"
+    
--- blog/views.py
+++ blog/views.py
@@ -26,23 +26,24 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
-    form = CommentForm()
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = Comment(
-                author=form.cleaned_data["author"],
+                author=request.user, 
                 body=form.cleaned_data["body"],
                 post=post,
             )
             comment.save()
             return HttpResponseRedirect(request.path_info)
+    else:
+        form = CommentForm() 
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
-        "form": CommentForm(),
+        "form": form,
     }
 
     return render(request, "blog/blog_detail.html", context)
-
