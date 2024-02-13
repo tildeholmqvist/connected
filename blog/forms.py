95c0ad80904297ba319cf2292cbffd0329fc7dcb
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-13 11:59:01 +0000

    trying to implement a working comment form

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, redirect
+from django import forms
+from .models import Post, Comment
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Your Name"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Leave a comment!"}
+        )
+    )
