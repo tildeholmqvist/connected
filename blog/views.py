39ec85a6e287b3e2f51b0c1e7d11e457d91b0452
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-15 12:12:52 +0000

    fixing my BlogList class in the views.py beacuse I cant see the posts for now

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Post, Comment
 from blog.forms import CommentForm
 from .forms import PostForm
 
+
 # Create your views here.
 
 class BlogList(generic.ListView):
     queryset = Post.objects.all().order_by("-created_at")
     template_name = "blog/index.html"
-    paginate_by = 6
+    context_object_name = "posts"
+    paginate_by =  6
 
 
-    
 def blog_category(request, category):
     posts = Post.objects.filter(
         categories__name__contains=category).order_by("-created_at")
@@ -24,9 +26,9 @@ def blog_category(request, category):
     return render(request, "blog/category.html", context)
 
 
-def blog_detail(request):
+def blog_detail(request, pk):
     queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset)
+    post = get_object_or_404(queryset, pk=pk)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(apporoved=True).count()
     if request.method == "POST":
