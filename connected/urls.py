049ee53367b8ab3cdf3362119c77207fb5204e4d
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-02-16 12:29:27 +0000

    adding a register/log out/log in part

--- connected/urls.py
+++ connected/urls.py
@@ -20,6 +20,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls")),
 ]
